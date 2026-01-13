173c3757eb00187a616addb910b29a4776a57301
Andre Luis Kunert <https://github.com/andreluiskunert/Curso_Angular_2_v19_2026>
2026-01-13 20:28:19 -0300

    bora codar com Angular_2026

--- projeto-angular/src/app/app.component.ts
+++ projeto-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,6 +8,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnChanges {
   title = 'projeto-angular';
+  constructor(){
+    console.log('Componente construtor')
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    // throw new Error('Method not implemented.')
+    console.log('Component Onchanges')
+  }
+  ngOnInit(): void {
+    console.log('Component OnInit')
+  }
 }
