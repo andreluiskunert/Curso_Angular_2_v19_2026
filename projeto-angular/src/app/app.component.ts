5cf8eee6cc00e77859b58070205579421192370b
Andre Luis Kunert <https://github.com/andreluiskunert/Curso_Angular_2_v19_2026>
2026-01-14 20:35:46 -0300

    bora la codar Angular_2026

--- projeto-angular/src/app/app.component.ts
+++ projeto-angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -8,10 +8,16 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentInit {
   title = 'projeto-angular';
   constructor(){
     console.log('Componente construtor')
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implement.')
+  }
+  ngAfterContentInit(): void {
+    console
   }
   ngOnChanges(changes: SimpleChanges): void {
     // throw new Error('Method not implemented.')
@@ -19,5 +25,13 @@ export class AppComponent implements OnInit, OnChanges {
   }
   ngOnInit(): void {
     console.log('Component OnInit')
+    // Quarta-feira,14/01/2026
+    this.title = 'Bora La Codar ';
+  }
+  ngDoCheck(): void {
+    console.log('Component DoCheck')
+  }
+  mudarTexto(){
+    this.title +=', Angular'
   }
 }
