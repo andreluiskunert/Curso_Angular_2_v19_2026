0d5495703a6c659f4a8c4b51f5f2115eb0ce46d0
Andre Luis Kunert <https://github.com/andreluiskunert/Curso_Angular_2_v19_2026>
2026-01-16 20:28:39 -0300

    bora la codar com Angular_2026

--- projeto-angular/src/app/app.component.ts
+++ projeto-angular/src/app/app.component.ts
@@ -1,17 +1,51 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+// import { Component } from "@angular/core";
+// import { RouterOutlet } from "@angular/router";
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.scss'],
+// })
+// export class AppComponent {
+//   title = 'Bem Vindo ao treinamento de Angulars';
+//   imgUrl = "https://www.altoros.com/blog/wp-content/uploads/2013/08/evaluation-of-angularjs-featured-image.png"
+//   constructor(){
+
+//   }
+//   chamarFuncao(){
+//     console.log("Esse é click...")
+//   }
+//   //  4 tipos de data binding:
+//   //  1ª Interpolação {{title}}
+//   //  2ª Property bind <img [src]="imgUrl">
+//   // 3ª Event bnding <button (click)="chamarFuncao()">
+//   // 4ª Two-way databinding <input [(ngModel)]="title">
+
+// }
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { FormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    FormsModule   // ✅ necessário para ngModel
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent  {
+export class AppComponent {
 
+  title = 'Bem Vindo ao treinamento de Angulars';
 
+  imgUrl = 'https://angular.io/assets/images/logos/angular/angular.png';
 
-  constructor(){
+  chamarFuncao() {
+    alert('Evento de clique funcionando!');
   }
-
-  // Pipes 
 
 }
