ae2fb81bd3a99ea288172fb9bceeba063a755530
Andre Luis Kunert <https://github.com/andreluiskunert/Curso_Angular_2_v19_2026>
2026-01-24 09:31:07 -0300

    bora la estudar AngularV19_2026

--- Projeto_Angular_ALTECNO_2026/projeto-1/src/app/app.component.ts
+++ Projeto_Angular_ALTECNO_2026/projeto-1/src/app/app.component.ts
@@ -1,36 +1,9 @@
-// import { AfterContentInit, Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.scss'
-// })
-// export class AppComponent {
-//   title = 'Bem ao treinamento de Angular';
-//   imgUrl= "https://img-c.udemycdn.com/course/750x422/6269961_7859_8.jpg";
-//   constructor(){
- 
-//   }
-//   chamarFuncao(){
-//     console.log("esse é um click...")
-//   }
-//  // 4 tipos de data Bindin:
-//   // inpterpolação
-//   // Property bind <img [src]="imgUrl">
-//   //  Event bind <button (click)="chamarFuncao"></button>
-//   //  Two-way databinding [(ngModel)]="title"
 
 
-
-// }
  import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -39,13 +12,16 @@ import { RouterOutlet } from '@angular/router';
     FormsModule,
     RouterOutlet
   ],
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  // styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
 
   title: string = 'Projeto Angular ALTECNO 2026';
   nome: string = '';
-  imgUrl: string = 'https://via.placeholder.com/300';
+  imgUrl: string = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN2US6o96hwPPpRSKcuTs9eVzH2nV1eM5IHg&s';
 
   chamarFuncao() {
     console.log('Função chamada!');
