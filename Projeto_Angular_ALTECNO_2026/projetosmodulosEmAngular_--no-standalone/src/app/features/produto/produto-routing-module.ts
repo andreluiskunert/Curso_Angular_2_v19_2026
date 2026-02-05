62365113fa8f851da07dcd014c5a8d21f96b109a
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-05 20:22:30 -0300

    bora la Estudar angular no FrontEnd_2026

--- Projeto_Angular_ALTECNO_2026/projetosmodulosEmAngular_--no-standalone/src/app/features/produto/produto-routing-module.ts
+++ Projeto_Angular_ALTECNO_2026/projetosmodulosEmAngular_--no-standalone/src/app/features/produto/produto-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomeComponent } from './home/home';
+import { NovoComponent } from './novo/novo';
+import { ListagemComponent } from './listagem/listagem';
+const routes: Routes = [
+  {path: '', redirectTo: 'listar', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'novo', component: NovoComponent},
+  {path: 'produto',}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
