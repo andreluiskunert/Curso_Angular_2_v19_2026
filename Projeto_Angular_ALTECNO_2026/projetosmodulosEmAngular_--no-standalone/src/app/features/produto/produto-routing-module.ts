4097d1a11264ec6f29e93e14300cb57814b037be
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-06 20:11:19 -0300

    bora la estudar com Angular no FrontEnd_2026

--- Projeto_Angular_ALTECNO_2026/projetosmodulosEmAngular_--no-standalone/src/app/features/produto/produto-routing-module.ts
+++ Projeto_Angular_ALTECNO_2026/projetosmodulosEmAngular_--no-standalone/src/app/features/produto/produto-routing-module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home';
-import { NovoComponent } from './novo/novo';
-import { ListagemComponent } from './listagem/listagem';
+
+import { HomeComponent } from './home/home.component';
+import { NovoComponent } from './novo/novo.component';
+import { ListagemComponent } from './listagem/listagem.component';
+
 const routes: Routes = [
-  {path: '', redirectTo: 'listar', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'novo', component: NovoComponent},
-  {path: 'produto',}
+  { path: '', redirectTo: 'listar', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'novo', component: NovoComponent },
+  { path: 'listar', component: ListagemComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule]   // 👈 MUITO IMPORTANTE
 })
-export class ProdutoRoutingModule { }
+export class ProdutoRoutingModule {}
