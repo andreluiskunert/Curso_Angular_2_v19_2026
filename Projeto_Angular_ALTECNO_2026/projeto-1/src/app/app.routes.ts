029ce121a0b835a06541cd21b4062623bb802b03
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-15 21:24:48 -0300

    bora Estudar Angular_2026

--- Projeto_Angular_ALTECNO_2026/projeto-1/src/app/app.routes.ts
+++ Projeto_Angular_ALTECNO_2026/projeto-1/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { NovoComponent } from './componentes/novo/novo.component';
 
 export const routes: Routes = [
-  
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent },
+  {path: 'novo', component: NovoComponent}
 ];
