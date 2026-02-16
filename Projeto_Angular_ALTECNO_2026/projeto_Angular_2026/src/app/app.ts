bdfc8fc5a568916cae46bd1f04ff8b8282ae216b
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-16 20:16:36 -0300

    bora la etudar angular_2026

--- Projeto_Angular_ALTECNO_2026/projeto_Angular_2026/src/app/app.ts
+++ Projeto_Angular_ALTECNO_2026/projeto_Angular_2026/src/app/app.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+
+import { Component, OnChanges, OnInit, signal, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,18 @@ import { Component, signal } from '@angular/core';
   standalone: false,
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit, OnChanges {
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
+
   protected readonly title = signal('projeto_Angular_2026');
+
+  ngOnInit(): void {
+    // Aqui você coloca sua lógica de inicialização
+    console.log('App iniciado com sucesso!');
+  }
+  mudarTexto() {
+    this.title.update(current => current + 'a');
+  }
 }
