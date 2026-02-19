5bf8a69a64ce02808a1d75cd8710c5c5941a4a88
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-19 20:31:47 -0300

    Bora La codar em Angular em FrontEnd_2026

--- Projeto_Angular_ALTECNO_2026/projeto_Angular_2026/src/app/app.ts
+++ Projeto_Angular_ALTECNO_2026/projeto_Angular_2026/src/app/app.ts
@@ -6,39 +6,44 @@ import { AfterContentInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, sig
   standalone: false,
   styleUrl: './app.scss'
 })
-export class App implements OnInit, OnChanges, DoCheck, OnDestroy , AfterContentInit{
-   frase: string = 'Curso Angular 2 v19 - 2026...bora construir um projeto incrível!';
+export class App //implements OnInit, OnChanges, DoCheck, OnDestroy , AfterContentInit//
+{
+   frase: string = '';
+   title = signal('projeto_Angular_2026');
 
-  ngAfterContentInit(): void {
-    throw new Error('Method not implemented.');
-  }
-  ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
-  }
+  // ngAfterContentInit(): void {
+  //   throw new Error('Method not implemented.');
+  // }
+  // ngOnDestroy(): void {
+  //   throw new Error('Method not implemented.');
+  // }
 
-  protected readonly title = signal('projeto_Angular_2026');
+  // protected readonly title = signal('projeto_Angular_2026');
 
-  ngOnInit(): void {
-    console.log('App iniciado com sucesso!');
-  }
+  // ngOnInit(): void {
+  //   console.log('App iniciado com sucesso!');
+  // }
 
-  ngOnChanges(_changes: SimpleChanges): void {
-    console.log('OnChanges chamado');
-    this.title.update(v => v + 'a');
-  }
+  // ngOnChanges(_changes: SimpleChanges): void {
+  //   console.log('OnChanges chamado');
+  //   this.title.update(v => v + 'a');
+  // }
 
-  ngDoCheck(): void {
-    console.log('DoCheck chamado');
-  }
+  // ngDoCheck(): void {
+  //   console.log('DoCheck chamado');
+  // }
 
-  mudarTexto() {
-    this.title.update(v => v + 'a');
-  }
+  // mudarTexto() {
+  //   this.title.update(v => v + 'a');
+  // }
   //  Quarta-feira, 19 de junho de 2024 - 19:52:00 h+|-
   //  Interpolação: {{ frase() }}
   // property binding: [innerText]="frase()"
   imgUrl:string = 'https://cdn.hashnode.com/res/hashnode/image/upload/v1701608958772/rx0OQPPos.png';
   // Two-way data binding: <input [(ngModel)]="frase">
+// ---//
+// quinta-feira,19/02/2026 _de noite
+// ngIf: *ngIf="frase"
 
 }
 
