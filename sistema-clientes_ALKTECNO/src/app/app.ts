c32b69baf9e82cfc7e75118c375eb5e8bcac0fd5
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-02-28 21:48:42 -0300

    bora la codar com  angular_2026

--- sistema-clientes_ALKTECNO/src/app/app.ts
+++ sistema-clientes_ALKTECNO/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnChanges, OnInit, signal, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,10 +7,19 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit, OnChanges{
  title = 'angular_2026';
  constructor(){
    console.log('compomente construtor')
  }
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  mudarTexto(){
+    this.title += 'A';
+  }
 }
 
