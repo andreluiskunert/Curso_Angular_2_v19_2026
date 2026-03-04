b3d6d959bf02504ebf6caf7f7933f47451b91da6
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-03-04 20:37:18 -0300

    bora la codar com Angular_2026

--- sistema-clientes_ALKTECNO/src/app/app.ts
+++ sistema-clientes_ALKTECNO/src/app/app.ts
@@ -16,8 +16,36 @@ export class App {
   imgUrl = 'https://miro.medium.com/v2/resize:fit:1400/1*WWRm1tIQ5MinLE0rUTo62w.png';
   MaisFrase = '';
   fraseIF = 'Bora Estudar Estruturas em Angular';
+  clientes = [
+    {
+      nome: 'André Luis',
+      sexo: 'Masculino',
+      idade: 43,
+      profissao: 'Dev Full Stack'
+    },
+       {
+      nome: 'Luis Carlos',
+      sexo: 'Masculino',
+      idade: 43,
+      profissao: 'Q.A'
+    },
+       {
+      nome: 'Antônio Luis',
+      sexo: 'Masculino',
+      idade: 43,
+      profissao: 'Dev Full Stack Python'
+    },
+       {
+      nome: 'Ana Luisa',
+      sexo: 'Feminino',
+      idade: 35,
+      profissao: 'Dev Full Stack Java'
+    },
+
+  ]
 
   chamarFuncao() {
     console.log('Este é um click');
   }
+
 }
