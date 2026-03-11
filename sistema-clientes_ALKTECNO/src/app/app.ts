7f325526bbc8b6708c1cca06b2027b835f7af319
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-03-11 20:42:07 -0300

    bora la estudar Angular.js

--- sistema-clientes_ALKTECNO/src/app/app.ts
+++ sistema-clientes_ALKTECNO/src/app/app.ts
@@ -6,18 +6,24 @@ import { Toobar } from './componentes/toobar/toobar';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet, FormsModule, CommonModule, Toobar],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
+
   title = 'angular_2026';
   frase = 'Bora Codar Angular';
+
   imgUrl = 'https://miro.medium.com/v2/resize:fit:1400/1*WWRm1tIQ5MinLE0rUTo62w.png';
+
   MaisFrase = '';
+
   fraseIF = 'Bora Estudar Estruturas em Angular';
 
   chamarFuncao() {
     console.log('Este é um click');
   }
+
 }
--- sistema-clientes_ALKTECNO/src/app/componentes/toobar/toobar.ts
+++ sistema-clientes_ALKTECNO/src/app/componentes/toobar/toobar.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { ProdutoRoutingModule } from "../../features/produto/produto-module";
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-toobar',
   standalone: true,
-  imports: [ProdutoRoutingModule],
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './toobar.html',
   styleUrl: './toobar.css'
 })
