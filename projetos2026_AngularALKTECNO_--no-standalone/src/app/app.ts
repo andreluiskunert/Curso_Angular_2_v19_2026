53746c140c492219891084e791fe5f38132c7d2f
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-03-14 15:23:30 -0300

    bora la codar com Angular_2026

--- projetos2026_AngularALKTECNO_--no-standalone/src/app/app.ts
+++ projetos2026_AngularALKTECNO_--no-standalone/src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
-export class App {
-  protected readonly title = signal('projetos2026_AngularALKTECNO_--no-standalone');
+export class App implements OnInit, OnChanges {
+  title = 'Angular_2026';
+
+  constructor() {
+    console.log('componentes de construtor...');
+  } 
+  // Fazer de noite
+  ngOnChanges(changes: SimpleChanges): void {
+    throw new Error('Method not implemented.');
+    console.log('bora ')
+  }
+
+  ngOnInit(): void {
+    console.log('componente iniciado...');
+  }
 }
