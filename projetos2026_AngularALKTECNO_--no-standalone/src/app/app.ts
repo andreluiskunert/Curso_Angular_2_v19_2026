76c4771ea2471e6ac46f9127df78636eaca22ff1
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-03-15 21:38:45 -0300

    bora la codar Angular_2026

--- projetos2026_AngularALKTECNO_--no-standalone/src/app/app.ts
+++ projetos2026_AngularALKTECNO_--no-standalone/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, DoCheck, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,11 +7,25 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
-export class App implements OnInit, OnChanges, DoCheck {
+export class App implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentInit, AfterViewInit, AfterViewChecked {
   title = 'Angular_2026';
 
   constructor() {
     console.log('componente no construtor...');
+  }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+    console.log('Component AfterViewInit')
+  }
+  ngAfterViewChecked(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngAfterContentInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+    console.log('Estou...')
   }
 
   ngDoCheck(): void {
