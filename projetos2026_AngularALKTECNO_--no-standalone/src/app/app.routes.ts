d56acefdba20d81cb6258f59fa5c229866347af3
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-03-24 20:42:03 -0300

    bora la codar com angular_2026

--- projetos2026_AngularALKTECNO_--no-standalone/src/app/app.routes.ts
+++ projetos2026_AngularALKTECNO_--no-standalone/src/app/app.routes.ts
@@ -3,7 +3,14 @@ import { Home } from './componentes/home/home';
 import { Novo } from './componentes/novo/novo';
 
 export const routes: Routes = [
-    {path:'', redirectTo: 'home', pathMatch: 'full'},
-    {path: 'home', component: Home},
-    {path: 'novo', component: Novo}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home },
+  { path: 'novo', component: Novo },
+
+  // Lazy loading
+  {
+    path: 'produto',
+    loadChildren: () =>
+      import('./features/produto/produto-module').then(m => m.ProdutoModule)
+  }
 ];
