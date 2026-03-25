d58069c467e626bc085b00edaf3a1e8dbc8980b7
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-03-25 20:44:53 -0300

    bora la embora daqui...

--- projetos2026_AngularALKTECNO_--no-standalone/src/app/features/produto/produto-module.ts
+++ projetos2026_AngularALKTECNO_--no-standalone/src/app/features/produto/produto-module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+// import { RouterModule, Routes } from '@angular/router';
 import { ProdutoRoutingModule } from './produto-routing-module';
+
+// // quarta-feira 25/03/2026
+// const routes: Routes = [
+//   {path: '', redirectTo: 'listagem', pathMatch: 'full'}
+// ]
 
 @NgModule({
   declarations: [],
   imports: [CommonModule, ProdutoRoutingModule],
 })
 export class ProdutoModule {}
+// fiz ontem esta
--- projetos2026_AngularALKTECNO_--no-standalone/src/app/features/produto/produto-routing-module.ts
+++ projetos2026_AngularALKTECNO_--no-standalone/src/app/features/produto/produto-routing-module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Listagem } from './listagem/listagem';
+import { Cadastro } from './cadastro/cadastro';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'listagem', pathMatch: 'full'},
+  {path: 'listagem', component: Listagem } ,
+  {path: 'novo-produto', component: Cadastro}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
