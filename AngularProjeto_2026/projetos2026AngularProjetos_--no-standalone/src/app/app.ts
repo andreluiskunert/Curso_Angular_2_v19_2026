02bdd368938c6e985321a3be2b17b950db66e975
Andre Luis Kunert <andreluiskunert@gmail.com>
2026-04-04 22:20:40 -0300

    bora la codar em Angular_2026

--- AngularProjeto_2026/projetos2026AngularProjetos_--no-standalone/src/app/app.ts
+++ AngularProjeto_2026/projetos2026AngularProjetos_--no-standalone/src/app/app.ts
@@ -1,17 +1,20 @@
+import { NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 import { AfterContentChecked, AfterContentInit, AfterViewInit, Component, DoCheck, OnChanges, OnDestroy, OnInit, signal, SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentChecked, AfterContentInit, AfterViewInit {
 
   title = 'Projeto01';
-  frase = "Bora Lá Codar";
+  frase = 'Bora Lá Codar';
+  frase01 ='';
+  frase02= 'codando...';
   imgUrl ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXp1flh-OGNg9PE2jrCKDgz4MylF8BZ5TmXA&s";
   chamarFuncao(){
     console.log("Esse é um click...")
@@ -51,5 +54,8 @@ export class App implements OnInit, OnChanges, DoCheck, OnDestroy, AfterContentC
   // obs.: Sabado,04/04/2026_De tarde
   // 3ª Event binding : "<button (click)="chamarFuncao()">Chamar Evento</button>"
   // 4ª Two-way databinding : "<input [(ngModel)]="frase"> "
-
+//  -------++-------
+//  Diretivas de decisão *ngIf *ngSwitch: Sabádo,04/04/2026_De noite 
+// 1ª ngIf: utilizado pra complemento else,utilizamos pra exibir ou esconder elementos tomando decisões lógicas...
+// 2ª ngSwitch: Comparação lógica na variavies 
 }
